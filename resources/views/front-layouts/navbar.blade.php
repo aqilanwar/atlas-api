8f1666241b6b8678f08f4e01193afb6ab54638da
Aqil Anwar <aqilanwarx@gmail.com>
2022-12-28 23:40:06 +0800

    courses page

--- resources/views/front-layouts/navbar.blade.php
+++ resources/views/front-layouts/navbar.blade.php
@@ -4,7 +4,7 @@
 
 
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="index.html" class="logo">
+      <a href="{{url('/')}}" class="logo">
         <img src="{{asset('assets/img/logo.png')}}" class="img-fluid" >
       </a>
 
@@ -12,28 +12,20 @@
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
+          <li><a class="nav-link scrollto" href="#services">Courses</a></li>
+          <li><a class="nav-link scrollto" href="#team">Teacher</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+
+          @if(Auth::user())
+            <li class="dropdown"><a href="#"><span>User</span> <i class="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="{{ route('profile')}}">Profile</a></li>
+                <li><a href="{{ route('logout')}}">Logout</a></li>
+              </ul>
+            </li>
+            @else
+            <li><a class="getstarted scrollto" href="{{route('login')}}">Login</a></li>
+            @endif
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
