044bfa117cb25dd44521481d28b3579a42de5d9b
Aqil Anwar <aqilanwarx@gmail.com>
2023-01-27 07:09:34 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
                 'name' => 'Admin', 
                 'email' => 'admin@gmail.com', 
                 'password' => Hash::make('password'),
-                'is_admin' => 0 ,
+                'is_admin' => 1 ,
             ],
 
         ];
